c23923b9a3e2259633e102cea764ff8fa756c009
xuzhou <xuzhou@janezt.com>
2025-07-16 20:05:39 +0800

    feat: playground更新

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,4 +1,20 @@
 import { createSSRApp } from 'vue'
+import { init } from '@fe-monitor/sdk'
+const { regenerateSessionId, reportLog } = init({
+  host: 'http://127.0.0.1:3001',
+  report: '/report',
+  pid: 'jz',
+  aid: 'JXGFSC',
+  uid: '1234567890',
+  maxBreadcrumbs: 5,
+  timeout: 1000,
+  retryTimes: 3
+})
+setTimeout(() => {
+  reportLog({
+    logContent: 'testtesttesttest'
+  })
+}, 5000)
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
